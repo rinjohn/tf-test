5a9641b7fd3e589d6cc8df819adc6b1d5a3110bd
rinjohn <43170752+rinjohn@users.noreply.github.com>
2024-10-23 13:58:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,20 +15,6 @@ provider "azurerm" {
   features {}
 }
 
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "gh-tf-storage-rg01"
-#     storage_account_name = "rjukstfstgacc01"
-#     container_name       = "poc1-tfstate"
-#     key                  = "terraform.tfstate"
-#     use_oidc             = true
-#     client_id            = "cb96d2df-220e-482d-a048-40ca97eb05e2"
-#     subscription_id      = "d425d56b-b751-47db-9b41-a7cffd203508"
-#     tenant_id            = "010af096-c756-4d39-9edd-a2607c57d24d"
-#     use_azuread_auth     = true
-#   }
-# }
-
 resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
